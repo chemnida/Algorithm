f0b083ce62c19f375387379051c18a8476690adb
chemni <chaemin9073@gmail.com>
2025-02-13 17:38:24 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.09 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int solution(String my_string, String is_prefix) {
-        return my_string.endsWith(is_prefix) ? 1 : 0;
+    public int solution(String my_string, String is_suffix) {
+        return my_string.endsWith(is_suffix) ? 1 : 0;
     }
 }
