bc92c2ddb6d5c4473adaae719854d4ed8f0d7f7e
chemni <chaemin9073@gmail.com>
2025-02-14 14:56:20 +0900

    [level 0] Title: l로 만들기, Time: 10.82 ms, Memory: 91.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String myString) {
+         StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < myString.length(); i++) {
+            char c = myString.charAt(i);
+            
+            if (c < 'l'){
+                result.append('l');
+            } else {
+                result.append(c);
+            }
+        } return result.toString();
+    }
+}
