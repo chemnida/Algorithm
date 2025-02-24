88b338265308e7b4a39e36d800388c4c7e8f3b8f
chemni <chaemin9073@gmail.com>
2025-02-24 15:26:26 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.10 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public List<String> solution(String[] str_list) {
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < str_list.length; i++) {
+            if (str_list[i].equals("l")) {
+                return Arrays.asList(Arrays.copyOfRange(str_list, 0, i));
+            } else if (str_list[i].equals("r")) {
+                return Arrays.asList(Arrays.copyOfRange(str_list, i + 1, str_list.length));
+            }
+        }
+
+        return result; // "l" 또는 "r"이 없으면 빈 리스트 반환
+    }
+}
